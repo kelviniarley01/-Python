2c452e01238663ce900f7f2500b651e6b5c86c5d
kelviniarley01 <kelviniarley6@gmail.com>
2025-09-23 20:35:03 -0300

    Add files via upload

---
+++ Requisitos funcionais exercício 6.py
@@ -0,0 +1,10 @@
+Requisitos funcionais exercício 6
+
+Cadastro de Cliente
+Cadastro de Peças no Estoque
+Criação de ordens de serviço
+Atualização do status da ordem
+Registro de serviços e valores
+Controle de datas de entrada e saída
+Listagem de ordens de serviço
+Geração de relatórios
